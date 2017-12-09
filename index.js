2cd1e02a72174c9e42a72aa17f68db3396daad4b
Jim McGee <jmcgee86@gmail.com>
2017-12-09 19:03:05 +0000

    displays list of works by artist with link to img

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ $(document).ready(function() {
 		method: "GET",
 		url: "https://www.rijksmuseum.nl/api/en/collection",
 		data: {
-            format: 'jsonp',
+            format: 'json',
 			key: APIKEY,
-			p: "10"
+			//p: "100",
+			ps: "100",
+			imgonly: "True",
+			type: "painting",
+			q: "Johannes vermeer"
 		},
 		success: function(data) {
-		 {
 				console.log(data);
 				console.log('test');
+				for (var i = 0; i < data.artObjects.length; i++) {
+							var item = document.createElement("LI");
+							var link = data.artObjects[i].links.web;
+							item.innerHTML = '<a target="_blank" href= "' + link + '">' + data.artObjects[i].title + '</a>';
+						//	var br = document.createElement("BR");
+							document.getElementById("work").appendChild(item);
+						//	document.getElementById("work").appendChild(br);
 			}
 		}
 	});
